959102d01065787cdc0598327f66273fda440f69
Sadman Chowdhury <sadman923@gmail.com>
2022-06-03 08:47:18 +0600

    finishing basic functionalities

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This example shows how to use [Tailwind CSS](https://tailwindcss.com/) [(v3.0)](https://tailwindcss.com/blog/tailwindcss-v3) with Next.js. It follows the steps outlined in the official [Tailwind docs](https://tailwindcss.com/docs/guides/nextjs).
 
+
 ## Deploy your own
 
 Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example) or preview live with [StackBlitz](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/with-tailwindcss)
@@ -21,3 +22,6 @@ pnpm create next-app --example with-tailwindcss with-tailwindcss-app
 ```
 
 Deploy it to the cloud with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) ([Documentation](https://nextjs.org/docs/deployment)).
+
+# Add sanity (Extra)
+yarn add next-sanity @portabletext/react @sanity/image-url
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link';
 import Header from './../components/Header';
+import { sanityClient, urlFor } from './../sanity';
+import { Post } from './../typings.d';
 
+interface Props {
+  posts: [Post];
+}
 
-const Home: NextPage = () => {
+export default function Home({ posts }: Props) {
+
   return (
     <div className='max-w-7xl mx-auto'>
       <Head>
@@ -29,9 +35,54 @@ const Home: NextPage = () => {
       </div>
 
       {/*Posts*/}
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+        {posts.map(post => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+                <img 
+                className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
+                src={
+                  urlFor(post.mainImage).url()
+                } alt=""/>
+                <div className='flex justify-between p-5 bg-white'>
+                  <div>
+                    <p className='text-lg font-bold'>{post.title}</p>
+                    <p>{post.description} by {post.author.name}</p>
+                  </div>
+                  <img className="h-12 w-12 rounded-full" src={urlFor(post.author.image).url()} alt="" />
+                </div>
+             </div>
+          </Link>
+        ))}
+
+      
+      </div>
 
     </div>
   )
 }
 
-export default Home
+
+export const getServerSideProps = async () => {
+  const query = `
+  *[_type == 'post' ] {
+    _id,
+    title,
+    slug,
+    author -> {
+    name,
+    image
+  }, 
+  description ,
+  mainImage,
+  slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
